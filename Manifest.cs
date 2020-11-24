550017fbf3cdeb410a00801ed3304bdc4a9bbaa9
Joe Berkley <joe.berkley@etchuk.com>
2020-11-24 14:40:12 +0000

    v0.1.1

--- Manifest.cs
+++ Manifest.cs
@@ -1,18 +1,18 @@
 using OrchardCore.Modules.Manifest;
 
-[assembly: Module(
+[assembly : Module (
     Author = "Etch UK.",
     Category = "Content",
     Description = "RSS feed to include meta title, image and description.",
     Name = "RSS",
-    Version = "0.1.0",
+    Version = "0.1.1",
     Website = "https://etchuk.com"
 )]
 
-[assembly: Feature(
+[assembly : Feature (
     Id = "Etch.OrchardCore.RSS",
     Name = "RSS",
     Description = "RSS feed to include meta title, image and description.",
     Category = "Content",
-    Dependencies = new[] { "Etch.OrchardCore.Fields.Query", "Etch.OrchardCore.SEO.MetaTags", "OrchardCore.ContentFields", "OrchardCore.Title" }
+    Dependencies = new [] { "Etch.OrchardCore.Fields.Query", "Etch.OrchardCore.SEO.MetaTags", "OrchardCore.ContentFields", "OrchardCore.Title" }
 )]
